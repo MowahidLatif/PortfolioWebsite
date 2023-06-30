b23948f103a26e52d2ef9ade29dda5f79739d832
mowahid latif <mowahidlatif@hotmail.com>
2023-06-30 18:11:32 -0500

    Update experience description

--- components/home/experience/Experience.tsx
+++ components/home/experience/Experience.tsx
@@ -19,11 +19,8 @@ const experience = [
     time: "November 2022 - May 2023",
     location: "Winnipeg, MB",
     description: [
-      "Performed research and wisely chose powerful tools to develop, improve, and manage 6+ complex projects for both clients and company usage thus greatly driving business expansion and creativity",
-      "Collaborated on strategic plans, overhauled 10+ client websites and apps, leveraged frontend/backend frameworks, and used cloud storage like AWS, Firebase, and Azure.",
-      "Reduced load-time by 60% and increased responsiveness 20% for client websites by implementing React.js and Next.js best practises.",
-      "Integrated AWS services to streamline CI/CD processes which increased project completion rate by 20% by leveraging Docker/Kubernetes to proficiently handle application deployment, scalability, and security.",
-      "Planned and created a custom agile setting involving troubleshooting/debugging code which resulted in a 30% faster shipment of code and reduced time in code review thus making debugging more effective.",
+      "In my role as a software developer, I undertook meticulous research and selected robust tools that were instrumental in the development, enhancement, and management of over six complex projects. These initiatives not only catered to our clients' needs but also served the company's internal operations. My efforts in making these judicious choices played a significant role in driving business growth and fostering a culture of creativity. I was also involved in the strategic planning and complete overhaul of more than ten client websites and applications. In doing so, I leveraged various frontend and backend frameworks and utilized cloud storage services such as AWS, Firebase, and Azure.",
+      "I was particularly successful in enhancing the performance of client websites by implementing best practices in React.js and Next.js. This led to a substantial 60% reduction in load time and a 20% improvement in responsiveness. Furthermore, I integrated AWS services to streamline CI/CD processes, effectively utilizing Docker and Kubernetes for the deployment, scalability, and security of applications. This integration resulted in a 20% increase in the project completion rate. To improve the efficiency of our development processes, I designed and implemented a custom agile setting for troubleshooting and debugging code. This setting expedited the code shipment by 30% and significantly reduced the time spent on code reviews, making debugging more effective."
     ],
     tech: [
       "React.js",
@@ -48,9 +45,8 @@ const experience = [
     time: "February 2022 - October 2022",
     location: "Winnipeg, MB",
     description: [
-      "Spearheaded the integration of C# and Python software in custom OEM hardware, enhancing existing products and contributing to a 30% acceleration in overall development time.",
-      "Collaborated with clients to design and engineer user-friendly interfaces using Python for customer-electronic products leading to increased hardware usability and reliability",
-      "Developed and maintained comprehensive software documentation, fostering an enriched code library that expedited future developments by 25% and contributed to a significant performance boost of 60% in specific hardware units.",
+      "At Smartrend Manufacturing Group, I had the opportunity to lead the integration of C# and Python software into custom OEM hardware. This initiative not only improved the existing product lineup but also resulted in a 30% acceleration of the overall development timeline. My role as a spearhead allowed me to effect significant changes in the production process and positively impacted the final output.",
+      "In a collaborative effort with our clients, I designed and engineered user-friendly interfaces for customer-electronic products using Python, which enhanced the usability and reliability of the hardware. This client-oriented approach was pivotal in developing solutions that were tailored to the end-users' needs and in turn increased product satisfaction. In addition to these, I took on the task of developing and maintaining extensive software documentation. This initiative enriched our code library, paving the way for future developments to be expedited by 25%. Not only did this systematic documentation provide a solid foundation for future projects, but it also led to a significant 60% performance boost in certain hardware units, underscoring the crucial role of comprehensive documentation in efficient software development.",
     ],
     tech: ["React.js", "Node.js", "C#", "Python", "Postgres", "NoSQL", "SQL"],
   },
@@ -60,11 +56,8 @@ const experience = [
     time: "October 2021 - December 2021",
     location: "Winnipeg, MB",
     description: [
-      "Engineered a Python-based Slack API Stack Bot, enhancing workflow automation, alert monitoring, and responsive communication",
-      "Leveraged Python to develop data analysis tools and create intuitive client interfaces using React.js, contributing to better data representation and system performance.",
-      "Successfully optimized the data pipeline, resulting in a 15% reduction in data processing time.",
-      "Implemented of Python-based data processing scripts significantly improved script quality by 30% and minimized coding errors.",
-      "Enhanced client interaction by developing an error-resistant, user-friendly GUI for the company website. Further employed Google APIs, such as Google Charts, Sheets, and Data Studio, to effectively present data to clients.",
+      "At my previous role, I engineered a Python-based Slack API Stack Bot that significantly enhanced workflow automation, alert monitoring, and responsive communication. This innovation was instrumental in improving our team's productivity and responsiveness to emergent issues. I also applied my Python skills to develop data analysis tools and used React.js to create intuitive client interfaces, contributing to better data representation and system performance.",
+      "In my pursuit of optimizing processes, I successfully streamlined the data pipeline, leading to a 15% reduction in data processing time. This optimization resulted in a more efficient workflow, enabling us to deliver results faster to our clients. Additionally, my implementation of Python-based data processing scripts substantially improved script quality by 30% and minimized coding errors, further enhancing the efficiency of our operations. To further elevate our client interaction, I developed an error-resistant, user-friendly GUI for the company website. I also utilized Google APIs, including Google Charts, Sheets, and Data Studio, to present data effectively to clients. This holistic approach to client interaction not only improved their experience but also enabled them to comprehend our data more effectively.",
     ],
     tech: [
       "Python",
@@ -78,12 +71,9 @@ const experience = [
     time: "July 2021 - September 2021",
     location: "Winnipeg, MB",
     description: [
-      "Leveraged React and Flutter to architect robust applications, integrating user-focused features and Machine Learning to enhance customer health predictions.",
-      "Enhanced coding efficiency through reusable code for bug resolution and reprogramming tasks, augmenting scalability.",
-      "Accelerated biometric data processing using Firebase, and collaborated on the creation of efficient, visually engaging analytical dashboards for clients.",
-      "Implemented of Python-based data processing scripts significantly improved script quality by 30% and minimized coding errors.",
-      "Enhanced client interaction by developing an error-resistant, user-friendly GUI for the company website. Further employed Google APIs, such as Google Charts, Sheets, and Data Studio, to effectively present data to clients.",
-    ],
+      "In my previous role, I exploited the capabilities of React and Flutter to build sturdy applications. These tools allowed me to embed user-centric functionalities and utilize Machine Learning, enabling improved predictions of customer health. In a bid to boost coding efficacy, I developed a strategy for code reuse in resolving bugs and reprogramming tasks, thereby enhancing our system's scalability and responsiveness to evolving demands and client specifications.",
+      "A significant accomplishment during my tenure involved employing Firebase to speed up the processing of biometric data. This initiative dramatically improved our ability to manage large data volumes in real-time. In a collaborative effort, my team and I developed efficient and visually appealing analytical dashboards for our clients, providing a comprehensive and user-friendly data overview. In tandem with these efforts, I implemented Python-based data processing scripts that notably improved script quality by 30% and reduced coding errors.",
+      "In a constant endeavor to improve client interactions, I developed a robust and user-friendly Graphical User Interface (GUI) for our company website. To present data to clients more effectively, I integrated Google APIs, including Google Charts, Sheets, and Data Studio. All these measures were aimed at ensuring a smooth and engaging client experience."    ],
     tech: [
       "React",
       "Flutter",
